ffd8897b4ddc16279fc20da24e5d416a83fe5e44
josmislas <josmislas12@gmail.com>
2023-01-31 17:07:53 -0600

    Proyecto Django

---
+++ myapp/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+
+class CreateNewTask(forms.Form):
+    title = forms.CharField(label="Titulo de Tarea", max_length=200,
+                            widget=forms.TextInput(attrs={'class': 'input'}))
+    description = forms.CharField(
+        label="Descripcion de la Tarea", widget=forms.Textarea(attrs={'class': 'input'}))
+
+
+class CreateNewProject(forms.Form):
+    name = forms.CharField(label="Nombre del Proyecto", max_length=200,
+                           widget=forms.TextInput(attrs={'class': 'input'}))
